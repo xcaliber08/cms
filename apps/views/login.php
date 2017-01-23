ad4f8f65612c455bd84ba8baaf83d7d2135c2f1d
Dev <Dev>
2017-01-23 10:12:02 +0800

    Add login page

---
+++ apps/views/login.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Login to Channel Management System</title>
+</head>
+<body>
+</body>
+</html>
